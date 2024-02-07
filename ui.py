7c140272674e2da79ed340da27b937102770e685
Ignacio Jorquera <ijorquera@alumnos.uai.cl>
2024-02-07 15:57:10 -0300

    Primer script casi completo

--- ui.py
+++ ui.py
@@ -19,11 +19,16 @@ class ConverterGUI:
         self.btn_convert_docx = tk.Button(root, text='Convert DOCX to PDF', command=self.convert_to_pdf)
         self.btn_convert_docx.pack(pady=20)
 
-def select_file():
-    file_path = filedialog.askopenfilename()
-    return file_path
+    def select_file(self):
+        file_path = filedialog.askopenfilename()
+        return file_path
 
-def convert_to_docx():
-    pdf_file = select_file()
-    if pdf_file:
-        # convert_pdf_to_docx()
+    def convert_to_docx(self):
+        pdf_file = self.select_file()
+        if pdf_file:
+            try:
+                result = convert_pdf_to_docx(pdf_file)
+                messagebox.showinfo('Conversion completed', f'The DOCX file has been created in {result}')
+            except Exception as e:
+                messagebox.showerror('Error', f'The conversion has failed: {e}')
+                
